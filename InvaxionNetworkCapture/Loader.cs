2397e28f4c4a4ed109f0ba7589db70957408dde1
MoeGrid <1065380934@qq.com>
2021-03-18 11:23:44 +0800

    添加抓包器

---
+++ InvaxionNetworkCapture/Loader.cs
@@ -0,0 +1,22 @@
+﻿using InvaxionNetworkHook.Hook;
+using System;
+
+namespace InvaxionNetworkHook
+{
+    public class Loader
+    {
+        public static void Main()
+        {
+            try
+            {
+                MyLogger.Log("Invaxion network capture!");
+
+                HookManager.getInstance().Create();
+            }
+            catch (Exception e)
+            {
+                MyLogger.Error(e.Message + "\n" + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# 音灵联网HOOK
+
+懒得写什么介绍了, 有能者自己看代码
+
+```
+InvaxionNetworkHook     Hook插件
+ProtoRestore            proto还原工具
+
+```
